709bddd676506902c2ca8000e460f1c823080b58
Musaddiq Askira <musaddiqaskira@gmail.com>
2023-01-23 23:36:25 +0200

    updated the look of the admin panel

--- recipe/admin.py
+++ recipe/admin.py
@@ -2,11 +2,18 @@ from django.contrib import admin
 
 from .models import Post, Category, Meal
 # Register your models here.
+
+
 admin.site.register(Category)
 admin.site.register(Meal)
-@admin.register(Post)
+admin.register(Post)
+admin.site.site_header = "CookWithMe Admin"
+admin.site.site_title = "CookWithMe Admin Portal"
+admin.site.index_title = "Welcome to CookWithMe Portal"
+
+
 class PostAdmin(admin.ModelAdmin):
-    list_display  = ('title', 'slug', 'author', 'publish', 'status')
+    list_display = ('title', 'slug', 'author', 'publish', 'status')
     list_filter = ('status', 'created', 'publish', 'author')
     search_fields = ('title', 'body')
     raw_id_fields = ('author',)
